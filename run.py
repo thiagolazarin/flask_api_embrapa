7afe5c6d41270893ff7f473346d4cbde254adf83
Thiago Rossi Lazarin <55093681+thiagolazarin@users.noreply.github.com>
2024-12-31 00:56:56 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
 from app import app
+from init_data import main as run_scraping
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def start_flask_server():
+    print("Iniciando servidor Flask...")
+    app.run(host="0.0.0.0", port=5000)  # Certifique-se de usar estas configurações para o Vercel
+
+if __name__ == "__main__":
+    # Passo 1: Executar o init_data.py (garantir que isso só rode na primeira inicialização)
+    print("Executando scraping e inicializando dados...")
+    try:
+        run_scraping()
+        print("Scraping concluído com sucesso!")
+    except Exception as e:
+        print(f"Erro ao executar o scraping: {str(e)}")
+
+    # Passo 2: Iniciar o servidor Flask
+    start_flask_server()
